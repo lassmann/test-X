b8f9d3175b9df316451e106f2c33ab3c02a192a8
Lucas Assmann <l.assmann.30@gmail.com>
2016-07-21 10:02:53 -0300

    access to resources

--- static/app.js
+++ static/app.js
@@ -1,4 +1,31 @@
-var app = angular.module("ascii-warehouse", []);
-app.controller("mainCtrl", function($scope) {
-    $scope.products = ["Milk", "Bread", "Cheese"];
-});
+angular.module("ascii-warehouse", [])
+    .controller("mainCtrl", function($scope, $http) {
+        $http({
+            headers: {"Accept":"application/json"},
+            method: 'GET',
+            url: '/api/products',
+            transformResponse: function Transformer(raw) {
+                var self = this;
+                self.result = [];
+                self.isEmptyString = function (str) {
+                    return (!str || 0 === str.length);
+                };
+                self.items = raw.split('\n');
+                angular.forEach(self.items, function Iterator (item) {
+                    if (!self.isEmptyString(item)) {
+                        var objectItem = JSON.parse(item);
+                        self.result.push(objectItem);
+                    }
+                });
+                return self.result;
+            }
+        }).then(function successCallback(response) {
+            console.log(response.data);
+            // this callback will be called asynchronously
+            // when the response is available
+        }, function errorCallback(response) {
+            console.log(response);
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    });
