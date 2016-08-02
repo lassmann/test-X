50521018a98b739cde758e9d70f368bc24c1dcdb
Lucas Assmann <l.assmann.30@gmail.com>
2016-08-01 21:13:42 -0300

    spinner added

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,9 @@
 var app = angular.module("ascii-warehouse", ['smart-table', 'ui.bootstrap', 'angularMoment'])
     .controller("mainCtrl", function ($scope, $http, Products) {
+        $scope.loading = true;
         Products.getAll()
             .then(function (products) {
-                moment.locale('es');
+                $scope.loading = false;
                 $scope.products = products;
             })
     });
