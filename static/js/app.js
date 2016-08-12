0b5d218992d7510695282a3ab8bb1f0bb9a34e89
lucas Assmann <l.assmann.30@gmail.com>
2016-08-12 18:01:32 -0300

    working the infiniteScroll

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module("ascii-warehouse", ['smart-table', 'ui.bootstrap', 'angularMoment'])
     .controller("mainCtrl", function ($scope, $http, Products) {
         $scope.loading = true;
-        Products.getAll(31)
+        Products.getAll(0)
             .then(function (products) {
                 $scope.products = products;
                 $scope.loading = false;
@@ -10,14 +10,10 @@ var app = angular.module("ascii-warehouse", ['smart-table', 'ui.bootstrap', 'ang
         $scope.loadMoreAscii = function (){
             Products.getAll(31)
                 .then(function (products) {
-                    console.log("bien entre", products);
-                    $scope.products.push(products);
-                    console.log($scope.products);
-                    // $scope.loading = false;
-                    // $scope.products = products;
+                    products.forEach(function (prod){
+                        $scope.products.push(prod);
+                    });
                 });
-
-            //console.log("entre bien por la funcion" );
         }
 
     });
