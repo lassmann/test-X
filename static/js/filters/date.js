3a5efcbfbb104710697bf31e6f682dd55cbf5978
lucas Assmann <l.assmann.30@gmail.com>
2016-08-18 16:02:53 -0300

    fix bug with the time

--- static/js/filters/date.js
+++ static/js/filters/date.js
@@ -1,6 +1,13 @@
 app.filter('date',function (){
     return function (input){
         moment.locale('es');
-        return moment(input).fromNow();
+        var actual = moment(new Date());
+        var date = moment(input);
+        var diff = actual.diff(date, 'days');
+        if(diff > 7){
+            return moment(input).format("YYYY-MM-DD");
+        }else{
+            return moment(input).fromNow();
+        }
     }
 });
